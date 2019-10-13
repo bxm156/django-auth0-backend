8561326ad1737e993c37af63f5f0fb6a29ef7857
Bryan Marty <bryan@bryanmarty.com>
2019-10-13 19:08:27 +0000

    update setup.py tox

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 
 setup(
     name='django-auth-backend',
+    author = "Bryan Marty",
+    keywords = "django python social auth auth0",
     version='0.1.0',
     packages=['django_auth0_backend'],
     license='MIT License',
@@ -13,5 +15,12 @@ setup(
     extra_requires=[
         'nameparser',
         'cryptography',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
     ],
 )
